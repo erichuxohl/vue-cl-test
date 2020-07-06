5c30713a6d973ef35a4a43bcd487e1258078b044
Eric Huxohl <eric.huxohl@miele.de>
2020-07-06 13:53:15 +0200

    Added files

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <Teaser />
+  </div>
+</template>
+
+<script>
+import Teaser from './components/Teaser.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Teaser
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/Teaser.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <span>this is a test teaser</span>
+    </div>
+</template>
+<script>
+export default {
+    name: 'teaser'
+}
+</script>
+<style scoped>
+span { color: red }
+</style>
